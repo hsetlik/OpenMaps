24223c7db71c0e586755c18d4e81ab87bc9a6a15
Hayden Setlik <haydensetlik@gmail.com>
2021-02-17 18:39:24 -0700

    initial commit

---
+++ jsx/aftereffects.jsx
@@ -0,0 +1,15 @@
+﻿#include "json2.js"
+
+function getPreviewPaths(path) {
+    }
+
+function osCheck() {
+        var os = $.os;
+        var match = os.indexOf("Windows");
+        if(match != (-1)) {
+                var userOS = "PC";
+            } else {
+                 var userOS = "MAC";
+                }
+            return userOS;
+    }
